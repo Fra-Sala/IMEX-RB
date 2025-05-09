da2c64cd83c61530442958c7d9c780097edaa282
Francesco Sala <francesco.sala@epfl.ch>
2025-05-09 11:03:11 +0200

    [CPU Times] The computation of CPU times work. Modified FE to deal with memory issues

--- src/euler.py
+++ src/euler.py
@@ -4,19 +4,35 @@ import scipy
 
 
 def forward_euler(problem, u0, tspan, Nt):
+    """
+    Forward Euler time integration scheme. Because of the stability requirement
+    it might not be possible to save an array of solutions for each time and each 
+    position in space. In such a case, only un and unp1 are saved, and the solution
+    at final time is returned.
+    """
     start = time.time()
     t0, tf = tspan
     dt = (tf - t0) / Nt
     tvec = np.linspace(t0, tf, Nt+1)
-    u = np.zeros((problem.N, Nt+1))
-    u[:, 0] = u0
+    try:
+        u = np.zeros((problem.N, Nt+1))
+        u[:, 0] = u0
+        un = u0
+        save_all = True
+    except MemoryError:
+        # Error: requested too much memory
+        # Save only current solution
+        un = u0
+    
     for n in range(Nt):
         # assemble rhs b
         b = problem.source_term(tvec[n])
         # We assemble all entries, even though the assembled
         # Dirichlet entries will be overwritten
-        u[:, n+1] = u[:, n] + dt * (problem.A @ u[:, n] + b)
-        u[:, n+1] = problem.enforce_bcs(u[:, n+1], tvec[n+1])
+        unp1 = un + dt * (problem.A @ un + b)
+        unp1 = problem.enforce_bcs(unp1, tvec[n+1])
+        if save_all: u[..., n+1] = unp1
+        un = unp1
     return u, tvec, time.time() - start
 
 
--- utils/errors.py
+++ utils/errors.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-def compute_errors(u, tvec, xvec, exact_solution, dim=1, q=2):
+def compute_errors(u, tvec, xvec, exact_solution, dim=1, q=2,
+                   finaltimeonly=False):
     """
     Compute FD errors over a dim dimensional domain.
     The formula for the error is taken from 
@@ -9,13 +10,22 @@ def compute_errors(u, tvec, xvec, exact_solution, dim=1, q=2):
     and Partial Differential Equations.'
 
     """
+    
+    if finaltimeonly:
+        # Consider only final time and final solution
+        tvec = tvec[-2:]
+        u = u[..., -1]
+    
     err_q_x = np.zeros(len(tvec) - 1)
 
     if dim == 1:
         dx = xvec[1] - xvec[0]  # Spatial step size
 
         for i in range(1, len(tvec)):
-            u_num = u[:, i]
+            if not finaltimeonly: 
+                u_num = u[:, i]
+            else:
+                u_num = u
             # Compute exact solution at time tvec[i]
             u_ex_mat = exact_solution(xvec, tvec[i])
             # Compute the error
