37b769430668eb145a2491a68e0d11ee3b1beabe
Francesco Sala <francesco.sala@epfl.ch>
2025-06-05 11:45:31 +0200

    [Renamed examples/ -> numerical_tests/]

--- numerical_tests/advDiff2D/config.py
+++ numerical_tests/advDiff2D/config.py
@@ -2,19 +2,21 @@
 
 import os
 
-Lx = 2  # length in x-direction
-Ly = 2  # length in y-direction
+Lx = 1  # length in x-direction
+Ly = 1  # length in y-direction
 
-Nx = 101   # number of elements in x-direction
-Ny = 101   # number of elements in y-direction
+# Ni_values = [51, 101]
+Ni_values = [21, 31]
+# Nx = 101   # number of elements in x-direction
+# Ny = 101   # number of elements in y-direction
 Nt = 100   # number of time steps
 
 mu = 0.02            # diffusion coefficient
-sigma = 0.5          # decay in the exact solution
-center = [0.5, 0.5]  # center of the exact solution exponential
+sigma = 0.25          # decay in the exact solution
+center = [0.25, 0.25]  # center of the exact solution exponential
 
-vx = 1.0  # advection velocity in x-direction
-vy = 0.5  # advection velocity in y-direction
+vx = 0.5  # advection velocity in x-direction
+vy = 0.25  # advection velocity in y-direction
 
 t0 = 0.0  # initial time
 T = 1.0   # final time
@@ -22,11 +24,11 @@ T = 1.0   # final time
 N = 10           # number of bases
 maxsubiter = 30  # maximal number of subiterations
 eps = 1e-4   # absolute stability condition for the IMEX-RB method
-
+tol_cond_NtFE = 1e-8
 sparse_solver = {"solver": "gmres", "typeprec": "ilu"}  # sparse solver configuration
 
 #######################################################################################################################
 
 project_path = os.getcwd()
-results_dir = os.path.join(project_path, "results")
+results_dir = os.path.join(project_path, "__RESULTS")
 os.makedirs(results_dir, exist_ok=True)
---
+++ numerical_tests/advDiff2D/test_EPSvsNt.py
@@ -0,0 +1,173 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                             '../..')))
+import numpy as np
+import timeit
+
+from src.problemsPDE import AdvDiff2D
+from src.euler import backward_euler, forward_euler
+from src.imexrb import imexrb
+from utils.helpers import integrate_1D, cond_sparse, create_test_directory, \
+    compute_steps_stability_FE
+from utils.errors import compute_errors
+
+from examples.advDiff2D.config import *
+
+import logging.config
+
+log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                             os.path.normpath('../../log.cfg'))
+logging.config.fileConfig(log_file_path, disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+
+def main():
+    """
+    In this test, we evaluate the performances of IMEX-RB, compared
+    to those of backward Euler, considering different timestep values.
+    We investigate the 2D advection diffusion problem
+    In particular, we study:
+    Convergence, average inner iter, and CPU time vs \\Delta t.
+    -- Multiple curves are produced varying EPSILON. --
+    We will then also plot the iterations over time for a given
+    \\Delta t.
+    """
+    # Define test parameters
+    Nx_values = Ni_values  # Nx = Ny
+    eps_values = np.array([100, 50, 10, 5, 1, 0.1])  # rel values of eps
+    Nt_values = np.array([2 ** n for n in range(4, 11)])  # range of Nt values
+
+    n_solves = 3  # number of solver calls to estimate computational times
+    # Initialise variables to track method performances
+    errors_l2 = {"IMEX-RB": np.empty((len(Nt_values), len(eps_values),
+                                      len(Nx_values))),
+                 "BE": np.empty((len(Nt_values), len(Nx_values))),
+                 "FE": np.empty((len(Nt_values), len(Nx_values)))}
+    errors_all = {"IMEX-RB": np.empty((len(Nt_values), len(eps_values),
+                                       Nt_values[-1], len(Nx_values))),
+                  "BE": np.empty((len(Nt_values), Nt_values[-1],
+                                  len(Nx_values))),
+                  "FE": np.empty((len(Nt_values), Nt_values[-1],
+                                  len(Nx_values)))}
+    times = {"IMEX-RB": np.zeros((len(Nt_values), len(eps_values),
+                                  len(Nx_values))),
+             "BE": np.zeros((len(Nt_values), len(Nx_values))),
+             "FE": np.empty((len(Nt_values), len(Nx_values)))}
+    subiters = {"IMEX-RB": np.empty((len(Nt_values), len(eps_values),
+                                     Nt_values[-1], len(Nx_values))),
+                "BE": None, "FE": None}
+    epsilon_refs = np.empty(len(Nx_values))
+    Nt_FEs = np.empty(len(Nx_values))
+    # Run simulations for different problem sizes
+    for idx_Nx, Nx in enumerate(Nx_values):
+        # Discretize equally along all dims
+        Ny = Nx
+        # Setup problem
+        problem = AdvDiff2D(Nx, Ny, Lx, Ly, mu=mu,
+                            sigma=sigma, vx=vx, vy=vy, center=center)
+        u0 = problem.initial_condition()
+        if idx_Nx == 0:
+            # Define test directory
+            testname = "EPSvsNt"
+            test_dir = create_test_directory(os.path.join(results_dir, problem.name),
+                                             testname)
+        epsilon_refs[idx_Nx] = 1.0 / cond_sparse(
+            problem.A,
+            tol=tol_cond_NtFE,
+            path=os.path.join(results_dir, problem.name,
+                            f"params_Nh_{problem.Nh}")
+        )
+
+        logger.debug(f"Considering reference epsilon = {epsilon_refs[idx_Nx]:.4e}")
+
+        Nt_FEs[idx_Nx] = compute_steps_stability_FE(
+            problem, [t0, T],
+            tol=tol_cond_NtFE,
+            path=os.path.join(results_dir, problem.name,
+                              f"params_Nh_{problem.Nh}")
+        )
+        for cnt_Nt, _Nt in enumerate(Nt_values):
+            print("\n")
+            logger.info(f"Solving for Nt={_Nt}")
+            tvec = np.linspace(t0, T, _Nt + 1)
+
+            logger.info("Solving with Backward Euler (BE)")
+            uBE, *_ = backward_euler(problem, u0, [t0, T], _Nt,
+                                     **sparse_solver)
+
+            if n_solves > 0:
+                f_BE = (lambda: backward_euler(problem, u0, [t0, T], _Nt,
+                                               **sparse_solver))
+                timer = timeit.Timer(f_BE)
+                _t = timer.repeat(number=1, repeat=n_solves)
+                times["BE"][cnt_Nt, idx_Nx] += np.mean(_t)
+
+            errors_all["BE"][cnt_Nt, :_Nt, idx_Nx] = compute_errors(
+                uBE, tvec, problem, mode="all"
+            )
+            errors_l2["BE"][cnt_Nt, idx_Nx] = integrate_1D(
+                errors_all["BE"][cnt_Nt, :_Nt, idx_Nx], tvec[1:]
+            )
+
+            logger.info("Solving with Forward Euler (FE)")
+            uFE, *_ = forward_euler(problem, u0, [t0, T], _Nt)
+
+            if n_solves > 0:
+                f_FE = (lambda: forward_euler(problem, u0, [t0, T], _Nt))
+                timer = timeit.Timer(f_FE)
+                _t = timer.repeat(number=1, repeat=n_solves)
+                times["FE"][cnt_Nt, idx_Nx] += np.mean(_t)
+
+            errors_all["FE"][cnt_Nt, :_Nt, idx_Nx] = compute_errors(
+                uFE, tvec, problem, mode="all"
+            )
+            errors_l2["FE"][cnt_Nt, idx_Nx] = integrate_1D(
+                errors_all["FE"][cnt_Nt, :_Nt, idx_Nx], tvec[1:]
+            )
+
+            logger.info("Solving with IMEX-RB")
+            # Run IMEX-RB for different epsilon values
+            for cnt_eps, eps in enumerate(eps_values):
+                epsilon = epsilon_refs[idx_Nx] * eps
+                logger.info(f"Solving for epsilon={epsilon:.4e}")
+
+                uIMEX, _, iters = imexrb(problem, u0, [t0, T], _Nt, epsilon, N,
+                                        maxsubiter)
+                if n_solves > 0:
+                    f_IMEX = lambda: imexrb(problem, u0, [t0, T], _Nt,
+                                            epsilon, N, maxsubiter)
+                    timer = timeit.Timer(f_IMEX)
+                    _t = timer.repeat(number=1, repeat=n_solves)
+                    times["IMEX-RB"][cnt_Nt, cnt_eps, idx_Nx] += np.mean(_t)
+
+                # Store subiterates
+                subiters["IMEX-RB"][cnt_Nt, cnt_eps, :_Nt, idx_Nx] = iters
+
+                # Compute errors
+                errors_all["IMEX-RB"][cnt_Nt, cnt_eps, :_Nt, idx_Nx] = compute_errors(
+                    uIMEX, tvec, problem, mode="all"
+                )
+                errors_l2["IMEX-RB"][cnt_Nt, cnt_eps, idx_Nx] = integrate_1D(
+                    errors_all["IMEX-RB"][cnt_Nt, cnt_eps, :_Nt, idx_Nx],
+                    tvec[1:]
+                )
+
+    # Save results
+    np.savez(
+        os.path.join(test_dir, "results.npz"),
+        Nt_FE=Nt_FEs,
+        errors_l2=errors_l2,
+        errors_all=errors_all,
+        times=times,
+        subiters=subiters,
+        eps_values=eps_values[:, np.newaxis] * epsilon_refs,
+        Nt_values=Nt_values,
+        Nx_values=Nx_values
+    )
+
+    return
+
+
+if __name__ == "__main__":
+    main()
